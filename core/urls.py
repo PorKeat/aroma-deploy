d2e74b8f295f3a7631c5ed96e41d70b364d87a56
PorKeat <alexkgm2412@gmail.com>
2025-08-03 01:07:47 +0700

    main

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,10 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import *
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 # DRF Router for ViewSets
 router = DefaultRouter()
@@ -76,3 +80,12 @@ urlpatterns = [
     path('shop/<int:id>/', views.product_details, name='productDetails'),
 ]
 
+# Serve /media/ for production (Render)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
+# Serve /media/ for development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
